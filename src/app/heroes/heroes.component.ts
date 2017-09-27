b850cf5b6e691ffba35dc84a8aa6b8eb8e7ab3b5
Dmitry Sichkar <dmmeteo@gmail.com>
2017-09-28 00:15:31 +0300

    routing list(need fix dashboard)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.sass']
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.sass']
 })
 export class HeroesComponent implements OnInit {
+    heroes: Hero[];
+    selectedHero: Hero;
 
-  constructor() { }
+    constructor(
+        private router: Router,
+        private heroService: HeroService) {}
 
-  ngOnInit() {
-  }
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes)
+    }
+
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+
+    gotoDetail(): void {
+        this.router.navigate(['/detail', this.selectedHero.id])
+    }
 
 }
