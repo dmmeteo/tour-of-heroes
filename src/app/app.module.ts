a5bee382a73ca89115128f7e3dc7686f3e25c938
Dmitry Sichkar <dmmeteo@gmail.com>
2017-10-06 23:08:14 +0300

    fixed module style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,8 @@ import {HttpModule} from '@angular/http';
 import {AppRoutingModule} from './app-routing.module';
 
 // Imports for loading & configuring the in-memory web api
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
-
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
 import {AppComponent} from './app.component';
 import {HeroesComponent} from './heroes/heroes.component';
 import {HeroDetailComponent} from './hero-detail/hero-detail.component';
